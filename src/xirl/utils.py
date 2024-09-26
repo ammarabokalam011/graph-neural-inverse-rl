efcaa705f76f9151f2b67fda1af66665d1b20603
Ammarabokalam0 <Ammarabokalam0@gmail.com>
2024-09-26 06:44:07 +0330

    change path

--- src/xirl/utils.py
+++ src/xirl/utils.py
@@ -24,7 +24,7 @@ from gym.wrappers import RescaleAction
 import matplotlib.pyplot as plt
 from ml_collections import config_dict
 import numpy as np
-from xirl.sac import wrappers, replay_buffer
+from xirl.sac import *
 import torch
 from torchkit import CheckpointManager
 from xirl.xirl import common
